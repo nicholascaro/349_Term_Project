89fa9937b8ac2659b441c354643a53c83e1abe2e
nicholascaro <70615231+nicholascaro@users.noreply.github.com>
2022-05-08 19:47:14 -0700

    create.js and edit.js

---
+++ client/src/components/create.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+export default function Create() {
+    const [form, setForm] = useState({
+        name: "", 
+        cost: 0, 
+        email: "", 
+    }); 
+
+    const navigate = useNavigate(); 
+
+    // update the state properties 
+
+    function updateForm(value){
+        return setForm((prev) => {
+            return { ...prev, ...value}; 
+        }); 
+    }
+
+    async function onSubmit(e){
+        e.preventDefault(); 
+
+        // When a post request is sent to the create url, we'll add a new subscription object to the database.
+        const newSubscription = { ...form}; 
+
+        await fetch("http://localhost:3000/createOne", {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json",
+            }, 
+            body: JSON.stringify(newSubscription),
+        })
+        .catch(error => {
+            window.alert(error)
+            return; 
+        }); 
+
+        setForm({name: "", cost: 0, email: ""}); 
+        navigate("/"); 
+    }
+
+    return (
+        <div>
+            <h3> Add a New Subscription</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name"> Subscription Name</label>
+                    <input 
+                    type= "text"
+                    className="form-control"
+                    id="name"
+                    value={form.name}
+                    onChange={(e) => updateForm({ name: e.target.value})}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="price"> Price of Subscription</label>
+                    <input 
+                    type= "text"
+                    className="form-control"
+                    id="cost"
+                    value={form.cost}
+                    onChange={(e) => updateForm({ name: e.target.value})}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email"> Email used for subscription</label>
+                    <input 
+                    type= "text"
+                    className="form-control"
+                    id="email"
+                    value={form.email}
+                    onChange={(e) => updateForm({ name: e.target.value})}
+                    />
+                </div>
+                <div className="form-group"> 
+                <input 
+                    type= "submit"
+                    value= "create subscription"
+                    className="btn btn-primary"
+                /> 
+                </div>
+            </form>
+        </div>
+    ); 
+}
---
+++ client/src/components/edit.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+
+export default function Edit() {
+    const [form, setForm] = useState({
+        name: "", 
+        cost: 0, 
+        email: "", 
+        records: []
+    }); 
+
+    const params =  useParams(); 
+    const navigate = useNavigate(); 
+
+    useEffect(() => {
+        async function fetchData() {
+            const id = params.id.toString(); 
+            const responce = await fetch(`http://localhost:3000/getOne/${params.id.toString()}`); 
+
+            if (!responce.ok){
+                const message = `Error occured: ${responce.statusText}`
+                window.alert(message); 
+                return; 
+            }
+
+            const subscription = await responce.json(); 
+            if (!subscription){
+                window.alert(`Record with id ${id} not found`); 
+                navigate("/"); 
+                return; 
+            }
+
+            setForm(subscription); 
+        }
+
+        fetchData(); 
+
+        return; 
+    }, [params.id, navigate]); 
+
+
+    // functions will be used to update the subscription properties
+    function updateForm(value) {
+        return setForm((prev) => {
+            return { ...prev, ...value}; 
+        }); 
+    }
+
+    async function onSubmit(e) {
+        e.preventDefault(); 
+        const editedSubscription = {
+            name: form.name, 
+            cost: form.cost,
+            email: form.email,
+        }; 
+
+        await fetch(`http://localhost:3000/updateOne/${params.id}`, {
+            method: "POST", 
+            body: JSON.stringify(editedSubscription), 
+            headers:{
+                "Content-Type": "application/json"
+            }, 
+        });
+
+        navigate("/"); 
+    }
+
+    // this form is where the user will provide input to update the object
+
+    return (
+        <div>
+            <h3> Update Subscription Details</h3>
+            <form onSubmit={onSubmit}>
+            <div className="form-group">
+                    <label htmlFor="name"> Subscription Name</label>
+                    <input 
+                    type= "text"
+                    className="form-control"
+                    id="name"
+                    value={form.name}
+                    onChange={(e) => updateForm({ name: e.target.value})}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="price"> Price of Subscription</label>
+                    <input 
+                    type= "text"
+                    className="form-control"
+                    id="cost"
+                    value={form.cost}
+                    onChange={(e) => updateForm({ name: e.target.value})}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email"> Email used for subscription</label>
+                    <input 
+                    type= "text"
+                    className="form-control"
+                    id="email"
+                    value={form.email}
+                    onChange={(e) => updateForm({ name: e.target.value})}
+                    />
+                </div>
+
+                <br /> 
+
+                <div className="form-group"> 
+                    <input 
+                        type = "submit"
+                        value = "Update Subscription"
+                        className="btn btn-primary"
+                    />
+                </div>
+            </form>
+        </div>
+    ); 
+}
