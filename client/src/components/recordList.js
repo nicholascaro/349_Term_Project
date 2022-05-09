71162b8806ae7d1f081b8cadadc1fd1ec7097fde
nicholascaro <70615231+nicholascaro@users.noreply.github.com>
2022-05-08 21:35:54 -0700

    recordList.js

---
+++ client/src/components/recordList.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react"; 
+import { Link } from "react-router-dom"
+
+const Subscription = (props) => (
+    <tr>
+        <td>{props.subscription.name}</td>
+        <td>{props.subscription.cost}</td>
+        <td>{props.subscription.email}</td>
+
+        <td>
+            < Link className="btn btn-link" to={`/edit/${props.subscription.id}`}> Update </Link> | 
+            <button className="btn btn-link"
+                onClick={() => {
+                    props.deleteSubscription(props.record._id); 
+                }}
+            > 
+            Delete Subscription
+            </button>
+        </td>
+    </tr>
+); 
+
+export default function SubscriptionList (){
+    const [subscriptions, setSubscriptions] = useState([]); 
+    
+    useEffect(() => {
+        async function getSubscriptions() {
+            const response = await fetch (`http://localhost:3000/getAll/`); 
+
+            if (!response.ok){
+                const message = `An error occured: ${response.statusText}`; 
+                window.alert(message); 
+                return; 
+            }
+
+            const subscriptions = await response.json(); 
+            setSubscriptions(subscriptions)
+        }
+
+        getSubscriptions(); 
+
+        return; 
+    }, [subscriptions.length]); 
+
+
+
+
+    async function deleteSubscription(id) {
+        await fetch(`http://localhost:300//deleleOne/${id}`, {
+            method: "DELETE"
+        }); 
+
+        const newSubscription = subscriptions.filter((el) => el._id !== id); 
+        setSubscriptions(newSubscription); 
+
+    }
+
+    // will present the subscriptions on a table
+
+    function subscriptionList() {
+        return subscriptions.map((subscription) => {
+            return (
+                <Subscription
+                subscription={subscription}
+                deleteSubscription={() => deleteSubscription(subscription._id)}
+                key={subscription._id}
+                />
+            ); 
+        }); 
+    }
+
+    return (
+        <div>
+            <h3> Subscription List</h3>
+            <table className="table table-striped" style={{marginTop: 20}}>
+                <thead>
+                    <tr>
+                        <th> Subscription Name </th>
+                        <th> Subscription Cost </th>
+                        <th> Subscription Email </th>
+                        <th> Changes </th>
+                    </tr>
+                </thead>
+                <tbody>{subscriptionList()}</tbody>
+            </table>
+        </div>
+    ); 
+    // This method will get the records from the database
+}
